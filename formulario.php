778590c38a3dd23e5e40d13e83f739a653d04dba
WildDes <124534731+WildDes@users.noreply.github.com>
2023-06-16 11:53:23 +0200

    formulario.php

--- formulario.php
+++ formulario.php
@@ -40,18 +40,14 @@ if ($_POST) {
     $apellido = $_POST['APELLIDO'];
     $email = $_POST['EMAIL'];
 
-/*Agrega esta línea para comprobar fallos, como me ha pasado a mí.*/
-    var_dump($nombre, $apellido, $email);
-
-/*Conexión con PDO. Son las variables necesarias para iniciar.*/
+/*Conexión con PDO.*/
 
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "cursosql";
 
-/*Creamos conexión con los datos que requiere PHP. Creamos una variable
-#con los parámetros necesarios para establecerla.*/
+/*Creamos conexión con los datos que requiere PHP.*/
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -66,8 +62,6 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO USUARIO (NOMBRE, APELLIDO, EMAIL)
 VALUES ('$nombre', '$apellido', '$email')";
 
-/*Si todo va bien, mensaje de éxito. Si no, se pide que
-se repita el proceso.*/
 
 if ($conn->query($sql) === TRUE) {
     echo "¡Registro creado con éxito! :)";
@@ -75,8 +69,6 @@ if ($conn->query($sql) === TRUE) {
     echo "¡Error!: " . $sql . "<br>" . $conn->error;
 }
 
-/*Cerramos la conexión. Lo ideal es ponerla, aunque las versiones
-recientes ya lo hace solo.*/
 
 $conn->close();
 
